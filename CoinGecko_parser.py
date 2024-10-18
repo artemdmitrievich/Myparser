c85be6334aad89a542561f8eeb95e9f350e20d19
Админ <artemdmitrievich25739180@gmail.com>
2024-10-18 19:30:10 +0500

    Добавил get_current_crypto_capitalization

--- CoinGecko_parser.py
+++ CoinGecko_parser.py
@@ -9,6 +9,8 @@ from bs4 import BeautifulSoup
 from time import time, sleep
 from urllib.parse import urljoin
 from static_for_all import *
+from transliterate import translit
+from langdetect import detect
 
 
 class General:
@@ -35,26 +37,38 @@ class General:
         responce_css = requests.get(url_css, headers=headers)
         soup_css = str(BeautifulSoup(responce_css.text, "lxml"))
         if decrease_in_capitalization in soup_css:
-            return f"Рыночная капитализация снизилась на {change_market_capitalization_value}"
+            return f"Рыночная капитализация криптовалюты снизилась на {change_market_capitalization_value}"
         else:
-            return f"Рыночная капитализация увеличилась на {change_market_capitalization_value}"
+            return f"Рыночная капитализация криптовалюты увеличилась на {change_market_capitalization_value}"
 
 
 class Crypto:
-    def get_current_crypto_price(crypto_name):
-        url_crypto = "https://bitinfocharts.com/ru/" + crypto_name.lower() + "/"
-        responce_crypto = requests.get(url_crypto)
-        soup_crypto = BeautifulSoup(responce_crypto.text, "lxml")
+
+    def __init__(self, crypto_name):
+        self.__crypto_name = (
+            crypto_name
+            if str(detect(crypto_name)) == "fi"
+            else str(translit(crypto_name, language_code="ru", reversed=True))
+            .replace("koin", "coin")
+            .replace("kesh", "cash")
+        )
+        self.__url_crypto = (
+            "https://bitinfocharts.com/ru/" + self.__crypto_name.lower() + "/"
+        )
+        self.__responce_crypto = requests.get(self.__url_crypto)
+        self.__soup_crypto = BeautifulSoup(self.__responce_crypto.text, "lxml")
+
+    def get_current_crypto_price(self):
         count = 0
         current_crypto_price = None
         while True:
             try:
                 current_crypto_price = (
-                    soup_crypto.find("td", id=f"tdid{count}")
+                    self.__soup_crypto.find("td", id=f"tdid{count}")
                     .find("a")
                     .find("span")
                     .find("span")
-                    .text
+                    .text.replace(",", " ")
                 )
             except:
                 count += 1
@@ -64,12 +78,27 @@ class Crypto:
 
         return current_crypto_price
 
+    def get_current_crypto_capitalization(self):
+        try:
+            current_crypto_capitalization = (
+                self.__soup_crypto.find(attrs={"style": "font-weight:bold"})
+                .text.replace("$", "")
+                .replace(",", " ")
+            )
+        except:
+            current_crypto_capitalization = "Нет информации"
+        return current_crypto_capitalization
+
 
 print(
     f"Рыночная капитализация криптовалюты = {General.get_data_market_capitalization()}$"
 )
 
-name_crypto = "bitcoin"
-print(f"Стоимость {name_crypto} = {Crypto.get_current_crypto_price(name_crypto)}$")
-
 print(General.get_change_market_capitalization())
+
+name_crypto = "биткоин кэш"
+print(f"Стоимость {name_crypto} = {Crypto(name_crypto).get_current_crypto_price()}$")
+
+print(
+    f"Рыночная капитализация {name_crypto} = {Crypto(name_crypto).get_current_crypto_capitalization()}$"
+)
