cd7fa616504c6b47956fb63372a4fc4e7584b12e
Админ <artemdmitrievich25739180@gmail.com>
2024-10-29 18:26:32 +0500

    Исправил вывод фунции get_popular_crypto, оптимизировал обновление главной страницы. Сделал заполняемую форму популярных криптовалют. Создал tests.py.

--- Main_info_crypto_parser.py
+++ Main_info_crypto_parser.py
@@ -4,11 +4,14 @@ from static.Soup_main_CoinGecko import coingecko_soup
 
 # Класс с общей информацией по криптовалюте
 class General:
+    def __init__(self):
+        pass
 
     # Получение общей капитализации криптовалюты
-    def get_data_market_capitalization():
+    def get_data_market_capitalization(self):
         data_market_capitalization = (
-            coingecko_soup().find(
+            coingecko_soup()
+            .find(
                 "div",
                 class_="tw-font-bold tw-text-gray-900 dark:tw-text-moon-50 tw-text-lg tw-leading-7",
             )
@@ -18,21 +21,33 @@ class General:
         return data_market_capitalization, strftime("%H:%M:%S", localtime())
 
     # Получение изменения общей капитализации криптовалюты за 24 часа
-    def get_change_market_capitalization():
-        capitalization_div = coingecko_soup().find("div", class_="tw-mt-1 tw-flex tw-flex-wrap tw-items-center tw-text-gray-500 dark:tw-text-moon-200 tw-font-semibold tw-text-sm tw-leading-5")
+    def get_change_market_capitalization(self):
+        capitalization_div = coingecko_soup().find(
+            "div",
+            class_="tw-mt-1 tw-flex tw-flex-wrap tw-items-center tw-text-gray-500 dark:tw-text-moon-200 tw-font-semibold tw-text-sm tw-leading-5",
+        )
         change_market_capitalization_value = 0
         if "gecko-down" in str(capitalization_div):
             direction = "down"
-            change_market_capitalization_value = capitalization_div.find("span", class_="gecko-down").text
+            change_market_capitalization_value = capitalization_div.find(
+                "span", class_="gecko-down"
+            ).text
         else:
             direction = "up"
-            change_market_capitalization_value = capitalization_div.find("span", class_="gecko-up").text
-        return change_market_capitalization_value, strftime("%H:%M:%S", localtime()), direction
+            change_market_capitalization_value = capitalization_div.find(
+                "span", class_="gecko-up"
+            ).text
+        return (
+            change_market_capitalization_value,
+            strftime("%H:%M:%S", localtime()),
+            direction,
+        )
 
     # Получение общего объёма торгов за 24 часа
-    def get_total_trading_volume_per_day():
+    def get_total_trading_volume_per_day(self):
         total_trading_volume_per_day = (
-            coingecko_soup().find(
+            coingecko_soup()
+            .find(
                 "div",
                 class_="tw-flex tw-flex-col tw-gap-2",
             )
@@ -49,7 +64,7 @@ class General:
 
 # Класс с дополнительной информацией по криптовалюте
 class Additional_CoinGecko_info:
-    
+
     def __init__(self):
         self.general_divs = coingecko_soup().find_all(
             "div",
@@ -63,7 +78,8 @@ class Additional_CoinGecko_info:
             class_="tw-flex tw-justify-between tw-px-2 tw-py-2.5 hover:tw-bg-gray-50 tw-rounded-lg dark:hover:tw-bg-moon-700",
         )
         dict_popular_crypto = {}
-        for item in class_popular_crypto:
+        for number in range(3):
+            item = class_popular_crypto[number]
             name_item = (
                 item.find(
                     "span",
@@ -81,18 +97,30 @@ class Additional_CoinGecko_info:
                 .find("span")
                 .text.replace("$", "")
             )
-            dict_popular_crypto[name_item] = name_item, price_item
-        return dict_popular_crypto
+            if "gecko-down" in item:
+                direction = "down"
+                change_value = item.find(
+                    "span",
+                    class_="gecko-down",
+                ).text
+            else:
+                direction = "up"
+                change_value = item.find(
+                    "span",
+                    class_="gecko-up",
+                ).text
+            dict_popular_crypto[number] = name_item, price_item, change_value, direction
+        return dict_popular_crypto, strftime("%H:%M:%S", localtime())
 
     # Получение информации по 3 криптовалютам с самым большим ростом за последние 24 часа по версии CoinGecko.com
     def get_greatest_growth_crypto(self):
-        class_popular_crypto = self.general_divs[1].find_all(
+        class_greatest_growth_crypto = self.general_divs[1].find_all(
             "div",
             class_="tw-flex tw-justify-between tw-px-2 tw-py-2.5 hover:tw-bg-gray-50 tw-rounded-lg dark:hover:tw-bg-moon-700",
         )
-        dict_popular_crypto = {}
+        dict_greatest_growth_crypto = {}
         for number in range(3):
-            item = class_popular_crypto[number]
+            item = class_greatest_growth_crypto[number]
             new_item = (
                 item.find(
                     "span",
@@ -114,9 +142,9 @@ class Additional_CoinGecko_info:
                 "span",
                 class_="gecko-up",
             ).text
-            dict_popular_crypto[number] = (
+            dict_greatest_growth_crypto[number] = (
                 new_item,
                 price_item,
                 growth_item,
             )
-        return dict_popular_crypto, strftime("%H:%M:%S", localtime())
+        return dict_greatest_growth_crypto, strftime("%H:%M:%S", localtime())
--- start_parser_gui.py
+++ start_parser_gui.py
@@ -17,26 +17,17 @@ class My_Ui_MainWindow(Ui_MainWindow):
         # Полное наследование от Ui_MainWindow
         super().setupUi(MainWindow)
 
-        # Заполнение значений в основных окнах
-        self.Value_capitalization_label.setText(
-            f"$ {General.get_data_market_capitalization()[0]}"
+        # Создание иконок trending_up и trending_down
+        self.Icon_trending_up = QtGui.QIcon()
+        self.Icon_trending_down = QtGui.QIcon()
+        self.Icon_trending_up.addPixmap(
+            QtGui.QPixmap("icons/trending_up.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off
         )
-        self.Time_update_capitalization_label.setText(
-            f"Обновлено в {General.get_data_market_capitalization()[1]}"
+        self.Icon_trending_down.addPixmap(
+            QtGui.QPixmap("icons/trending_down.png"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.Off,
         )
-        self.Value_volume_label.setText(
-            f"$ {General.get_total_trading_volume_per_day()[0]}"
-        )
-        self.Time_update_volume_label.setText(
-            f"Обновлено в {General.get_total_trading_volume_per_day()[1]}"
-        )
-        self.Value_change_capitalization_label.setText(
-            self.__Create_output_to_value_change_capitalization_label()
-        )
-        self.Time_update_change_capitalization_label.setText(
-            f"Обновлено в {General.get_change_market_capitalization()[1]}"
-        )
-        self.Update_Main_Button.clicked.connect(self._Update_main_page)
 
         # Создание ссылки на coingecko.com
         self.Value_information_source_label.setText(
@@ -60,20 +51,176 @@ class My_Ui_MainWindow(Ui_MainWindow):
             lambda: webbrowser.open(donates_url)
         )
 
-        # Создание иконок trending_up и trending_down
-        self.Icon_trending_up = QtGui.QIcon()
-        self.Icon_trending_down = QtGui.QIcon()
-        self.Icon_trending_up.addPixmap(
-            QtGui.QPixmap("icons/trending_up.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off
+        # Обработка кнопок обновления страницы
+        self.Update_Main_Button.clicked.connect(self._Update_main_page)
+        self.Update_Additional_Button.clicked.connect(self._Update_additional_page)
+
+        # Сразу при инициализации обновляем main и additional page
+        self._Update_main_page()
+        self._Update_additional_page()
+
+    # Функция обновления страницы additional
+    def _Update_additional_page(self):
+        Current_additional = Additional_CoinGecko_info()
+
+        # Обновление таблицы криптовалют, с наибольшим ростом
+        Current_greatest_growth_crypto = Current_additional.get_greatest_growth_crypto()
+        # Обновление названий
+        if len(Current_greatest_growth_crypto[0][0][0]) > 18:
+            self.Max_growth_name_label_1.setText(
+                Current_greatest_growth_crypto[0][0][0][0:15] + "..."
+            )
+        else:
+            self.Max_growth_name_label_1.setText(
+                " " * ((18 - len(Current_greatest_growth_crypto[0][0][0])) // 2)
+                + Current_greatest_growth_crypto[0][0][0]
+            )
+        if len(Current_greatest_growth_crypto[0][1][0]) > 18:
+            self.Max_growth_name_label_2.setText(
+                Current_greatest_growth_crypto[0][1][0][0:15] + "..."
+            )
+        else:
+            self.Max_growth_name_label_2.setText(
+                " " * ((18 - len(Current_greatest_growth_crypto[0][0][0])) // 2)
+                + Current_greatest_growth_crypto[0][1][0]
+            )
+        if len(Current_greatest_growth_crypto[0][2][0]) > 18:
+            self.Max_growth_name_label_3.setText(
+                Current_greatest_growth_crypto[0][2][0][0:15] + "..."
+            )
+        else:
+            self.Max_growth_name_label_3.setText(
+                " " * ((18 - len(Current_greatest_growth_crypto[0][0][0])) // 2)
+                + Current_greatest_growth_crypto[0][2][0]
+            )
+        # Обновление времени
+        self.Max_growth_time_label_1.setText(
+            " " * 12 + Current_greatest_growth_crypto[1]
         )
-        self.Icon_trending_down.addPixmap(
-            QtGui.QPixmap("icons/trending_down.png"),
-            QtGui.QIcon.Normal,
-            QtGui.QIcon.Off,
+        self.Max_growth_time_label_2.setText(
+            " " * 12 + Current_greatest_growth_crypto[1]
         )
+        self.Max_growth_time_label_3.setText(
+            " " * 12 + Current_greatest_growth_crypto[1]
+        )
+        # Обновление стоимости
+        self.Max_growth_cost_label_1.setText(
+            " " * 8 + "$ " + Current_greatest_growth_crypto[0][0][1]
+        )
+        self.Max_growth_cost_label_2.setText(
+            " " * 8 + "$ " + Current_greatest_growth_crypto[0][1][1]
+        )
+        self.Max_growth_cost_label_3.setText(
+            " " * 8 + "$ " + Current_greatest_growth_crypto[0][2][1]
+        )
+        # Обновление увеличения
+        self.Max_growth_change_label_1.setText(
+            " " * 7 + Current_greatest_growth_crypto[0][0][2]
+        )
+        self.Max_growth_change_label_2.setText(
+            " " * 7 + Current_greatest_growth_crypto[0][1][2]
+        )
+        self.Max_growth_change_label_3.setText(
+            " " * 7 + Current_greatest_growth_crypto[0][2][2]
+        )
+
+        # Обновление таблицы самых популярных криптовалют
+        Current_popular_crypto = Current_additional.get_popular_crypto()
+        # Обновление названий
+        if len(Current_popular_crypto[0][0][0]) > 18:
+            self.Popular_name_label_1.setText(
+                Current_popular_crypto[0][0][0][0:15] + "..."
+            )
+        else:
+            self.Popular_name_label_1.setText(
+                " " * ((18 - len(Current_popular_crypto[0][0][0])) // 2)
+                + Current_popular_crypto[0][0][0]
+            )
+        if len(Current_popular_crypto[0][1][0]) > 18:
+            self.Max_growth_name_label_2.setText(
+                Current_popular_crypto[0][1][0][0:15] + "..."
+            )
+        else:
+            self.Popular_name_label_2.setText(
+                " " * ((18 - len(Current_popular_crypto[0][0][0])) // 2)
+                + Current_popular_crypto[0][1][0]
+            )
+        if len(Current_popular_crypto[0][2][0]) > 18:
+            self.Popular_name_label_3.setText(
+                Current_popular_crypto[0][2][0][0:15] + "..."
+            )
+        else:
+            self.Popular_name_label_3.setText(
+                " " * ((18 - len(Current_popular_crypto[0][0][0])) // 2)
+                + Current_popular_crypto[0][2][0]
+            )
+        # Обновление времени
+        self.Popular_time_label_1.setText(" " * 12 + Current_popular_crypto[1])
+        self.Popular_time_label_2.setText(" " * 12 + Current_popular_crypto[1])
+        self.Popular_time_label_3.setText(" " * 12 + Current_popular_crypto[1])
+        # Обновление стоимости
+        self.Popular_cost_label_1.setText(
+            " " * 8 + "$ " + Current_popular_crypto[0][0][1]
+        )
+        self.Popular_cost_label_2.setText(
+            " " * 8 + "$ " + Current_popular_crypto[0][1][1]
+        )
+        self.Popular_cost_label_3.setText(
+            " " * 8 + "$ " + Current_popular_crypto[0][2][1]
+        )
+        # Обновление значений изменения стоимости
+        self.Popular_change_label_1.setText(" " * 7 + Current_popular_crypto[0][0][2])
+        self.Popular_change_label_2.setText(" " * 7 + Current_popular_crypto[0][1][2])
+        self.Popular_change_label_3.setText(" " * 7 + Current_popular_crypto[0][2][2])
+        # Вызов функции для обновления иконок и стилей у самых популярных криптовалют
+        self.__Create_Icon_and_Style(Current_popular_crypto)
+
+    # Обновляет иконки и стили у самых популярных криптовалют
+    def __Create_Icon_and_Style(self, Current_popular_crypto):
+        if Current_popular_crypto[0][0][3] == "down":
+            self.Popular_change_label_1.setStyleSheet(
+                "color: rgb(255,0,0);\n"
+                "font-size: 16px;\n"
+                "font-weight: bold;\n"
+                "background-color: rgb(157, 0, 255);"
+            )
+            self.Popular_Icon_Button_1.setIcon(self.Icon_trending_down)
+        if Current_popular_crypto[0][1][3] == "down":
+            self.Popular_change_label_2.setStyleSheet(
+                "color: rgb(255,0,0);\n"
+                "font-size: 16px;\n"
+                "font-weight: bold;\n"
+                "background-color: rgb(157, 0, 255);"
+            )
+            self.Popular_Icon_Button_2.setIcon(self.Icon_trending_down)
+        if Current_popular_crypto[0][2][3] == "down":
+            self.Popular_change_label_3.setStyleSheet(
+                "color: rgb(255,0,0);\n"
+                "font-size: 16px;\n"
+                "font-weight: bold;\n"
+                "background-color: rgb(157, 0, 255);"
+            )
+            self.Popular_Icon_Button_3.setIcon(self.Icon_trending_down)
+
+    # Обработка нажатия кнопки "Нажмите, чтобы обновить" на главной странице
+    # Срабатывает в промежутке времени от 5 до 8 секунд
+    def _Update_main_page(self):
+
+        # Создание текущего экземпляра класса и вызов функций
+        Current_General = General()
+        Current_data_market_capitalization = (
+            Current_General.get_data_market_capitalization()
+        )
+        Current_change_market_capitalization = (
+            Current_General.get_change_market_capitalization()
+        )
+        Current_total_trading_volume_per_day = (
+            Current_General.get_total_trading_volume_per_day()
+        )
+
         # Проверка направления изменения рыночной капитализации
         # и установка нужной иконки тренда
-        if General.get_change_market_capitalization()[2] == "up":
+        if Current_change_market_capitalization[2] == "up":
             self.Direction_change_capitalization_Icon_Button.setIcon(
                 self.Icon_trending_up
             )
@@ -88,86 +235,7 @@ class My_Ui_MainWindow(Ui_MainWindow):
                 QtCore.QSize(40, 40)
             )
 
-        self.Update_Additional_Button.clicked.connect(self._Update_additional_page)
-
-        # Сразу при инициализации обновляем additional page
-        self._Update_additional_page()
-
-    # Функция обновления страницы additional
-    def _Update_additional_page(self):
-        Current_additional = Additional_CoinGecko_info()
-
-        # Обновление таблицы криптовалют, с наибольшим ростом
-        Current_greatest_growth_crypto = Current_additional.get_greatest_growth_crypto()
-        if len(Current_greatest_growth_crypto[0][0][0]) > 18:
-            self.Max_growth_name_label_1.setText(
-                Current_greatest_growth_crypto[0][0][0][0:17] + "..."
-            )
-        else:
-            self.Max_growth_name_label_1.setText(
-                " " * ((18 - len(Current_greatest_growth_crypto[0][0][0])) // 2)
-                + Current_greatest_growth_crypto[0][0][0]
-            )
-        if len(Current_greatest_growth_crypto[0][1][0]) > 18:
-            self.Max_growth_name_label_2.setText(
-                Current_greatest_growth_crypto[0][1][0][0:17] + "..."
-            )
-        else:
-            self.Max_growth_name_label_2.setText(
-                " " * ((18 - len(Current_greatest_growth_crypto[0][0][0])) // 2)
-                + Current_greatest_growth_crypto[0][1][0]
-            )
-        if len(Current_greatest_growth_crypto[0][2][0]) > 18:
-            self.Max_growth_name_label_3.setText(
-                Current_greatest_growth_crypto[0][2][0][0:17] + "..."
-            )
-        else:
-            self.Max_growth_name_label_3.setText(
-                " " * ((18 - len(Current_greatest_growth_crypto[0][0][0])) // 2)
-                + Current_greatest_growth_crypto[0][2][0]
-            )
-        self.Max_growth_time_label_1.setText(
-            " " * 12 + Current_greatest_growth_crypto[1]
-        )
-        self.Max_growth_time_label_2.setText(
-            " " * 12 + Current_greatest_growth_crypto[1]
-        )
-        self.Max_growth_time_label_3.setText(
-            " " * 12 + Current_greatest_growth_crypto[1]
-        )
-        self.Max_growth_cost_label_1.setText(
-            " " * 8 + "$ " + Current_greatest_growth_crypto[0][0][1]
-        )
-        self.Max_growth_cost_label_2.setText(
-            " " * 8 + "$ " + Current_greatest_growth_crypto[0][1][1]
-        )
-        self.Max_growth_cost_label_3.setText(
-            " " * 8 + "$ " + Current_greatest_growth_crypto[0][2][1]
-        )
-        self.Max_growth_change_label_1.setText(
-            " " * 7 + Current_greatest_growth_crypto[0][0][2]
-        )
-        self.Max_growth_change_label_2.setText(
-            " " * 7 + Current_greatest_growth_crypto[0][1][2]
-        )
-        self.Max_growth_change_label_3.setText(
-            " " * 7 + Current_greatest_growth_crypto[0][2][2]
-        )
-
-    # Обработка нажатия кнопки "Нажмите, чтобы обновить" на главной странице
-    # Срабатывает в промежутке времени от 5 до 8 секунд
-    def _Update_main_page(self):
-        # lambda: self.start_animation()
-        Current_General = General
-        Current_data_market_capitalization = (
-            Current_General.get_data_market_capitalization()
-        )
-        Current_change_market_capitalization = (
-            Current_General.get_change_market_capitalization()
-        )
-        Current_total_trading_volume_per_day = (
-            Current_General.get_total_trading_volume_per_day()
-        )
+        # Установка основныз значений
         self.Value_capitalization_label.setText(
             f"$ {Current_data_market_capitalization[0]}"
         )
@@ -179,15 +247,16 @@ class My_Ui_MainWindow(Ui_MainWindow):
             f"Обновлено в {Current_total_trading_volume_per_day[1]}"
         )
         self.Value_change_capitalization_label.setText(
-            self.__Create_output_to_value_change_capitalization_label()
+            self.__Create_output_to_value_change_capitalization_label(
+                Current_change_market_capitalization
+            )
         )
         self.Time_update_change_capitalization_label.setText(
             f"Обновлено в {Current_change_market_capitalization[1]}"
         )
 
     # Создание текста для обновления изменения рыночной капитализации
-    def __Create_output_to_value_change_capitalization_label(self):
-        Current = General.get_change_market_capitalization()
+    def __Create_output_to_value_change_capitalization_label(self, Current):
         if Current[2] == "down":
             direction = "Понизилась на"
         else:
